76d5c24e8914c984be47d72c2ba74a6287b48bdb
Ross <ross@duris.io>
2023-03-30 19:19:15 -0400

    update env

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -8,9 +8,11 @@ export default function Login() {
   const router = useRouter();
 
   const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
     fetch("http://localhost:3001/api/login", {
       method: "post",
       credentials: "include",
+      mode: "cors",
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
@@ -30,23 +32,25 @@ export default function Login() {
     <>
       <h1>Login</h1>
 
-      <div>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button onClick={handleSubmit}>Submit</button>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
